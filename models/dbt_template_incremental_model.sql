dbd9673474ec437ca95d50bc68dd7cc593d1833d
jeff-dude <jeff@dune.xyz>
2025-10-02 15:58:14 -0400

    fix incremental model

--- models/dbt_template_incremental_model.sql
+++ models/dbt_template_incremental_model.sql
@@ -9,8 +9,8 @@
     , alias = 'incremental_model'
     , materialized = 'incremental'
     , incremental_strategy = 'merge'
-    , unique_key = ['block_number']
-    , incremental_predicates = ["DBT_INTERNAL_DEST.block_date >= now() - interval '1' hour"]
+    , unique_key = ['block_number', 'block_date']
+    , incremental_predicates = ["DBT_INTERNAL_DEST.block_date >= now() - interval '1' day"]
     , on_table_exists = 'replace'
 )
 }}
@@ -24,9 +24,9 @@ from
 where
     1 = 1
     {%- if is_incremental() %}
-    AND block_date >= now() - interval '1' hour -- on incremental runs, we only want to process the last hour of data
+    AND block_date >= now() - interval '1' day -- on incremental runs, we only want to process the last day of data
     {%- else %}
-    AND block_date >= now() - interval '1' day -- on full refresh runs, we want to process all data (to expedite, keep one day's worth of data)
+    AND block_date >= now() - interval '7' day -- on full refresh runs, we want to process all data (to expedite, keep one week's worth of data)
     {%- endif %}
 group by
     block_number
