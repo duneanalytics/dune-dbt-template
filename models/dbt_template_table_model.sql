112d2aaa9fafdbab473a0f42fd343e5ff8d24e89
jeff-dude <jeff@dune.xyz>
2025-10-03 12:30:30 -0400

    cleanup comments

--- models/dbt_template_table_model.sql
+++ models/dbt_template_table_model.sql
@@ -1,7 +1,5 @@
 {#
     key notes on table model:
-    - on_table_exists = 'replace' is used to replace the table if it already exists
-        - dunesql hive metastore does *not* allow rename of table, meaning standard dbt table operations won't work (drop temp table, create temp table, rename existing table to backup, rename temp to final, drop backup)
     - file_format defaults to delta (TODO: confirm this is dune hive metastore setting)
         - when providing file_format config to model, dbt fails on unable to support 'format' property
 #}
