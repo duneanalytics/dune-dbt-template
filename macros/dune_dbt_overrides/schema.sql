6128b8693b91e8d359fb3abc114a9ca63bf73b4f
jeff-dude <102681548+jeff-dude@users.noreply.github.com>
2025-10-03 12:35:12 -0400

    make replace config global, simplify temp macro override for schema with location property (#5)
    * make replace table global
    
    * simplify schema with location macro
    
    * cleanup comments

--- macros/dune_dbt_overrides/schema.sql
+++ macros/dune_dbt_overrides/schema.sql
@@ -1,20 +1,11 @@
 {#
-    goal: remove this macro override, have the backend handle s3 bucket names
+    TODO: remove this macro override, have the backend handle s3 bucket names
       - prod: trino-prod-datasets-118330671040/write/<customer_team_name>/…
       - dev:  trino-dev-datasets-118330671040/write/<customer_team_name>/…
-    goal: remove the trino__create_schema macro override and ensure backend applies <WITH location> logic
 #}
-
-{%- macro s3_bucket() -%}
-  {%- if target.name == 'prod' -%}
-    {{- return('prod-spellbook-trino-118330671040') -}}
-  {%- else -%}
-    {{- return('trino-dev-datasets-118330671040') -}}
-  {%- endif -%}
-{%- endmacro -%}
 
 {% macro trino__create_schema(relation) -%}
   {%- call statement('create_schema') -%}
-   CREATE SCHEMA {{ relation }} WITH (location = 's3a://{{s3_bucket()}}/')
+   CREATE SCHEMA {{ relation }} WITH (location = 's3a://prod-spellbook-trino-118330671040/{{relation}}/')
   {% endcall %}
 {% endmacro %}
