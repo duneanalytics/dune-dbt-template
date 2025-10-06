f9368df19b90accd74da08ebdef582fc16532d3e
jeff-dude <jeff@dune.xyz>
2025-10-06 18:26:47 -0400

    temp: force test_schema

--- macros/dune_dbt_overrides/get_custom_schema.sql
+++ macros/dune_dbt_overrides/get_custom_schema.sql
@@ -5,21 +5,29 @@
         - <team_name>__tmp_<dev_name> : use this for dev deployments by a specific developer or PR
 #}
 
+{#
+    TEMP: force `test_schema` for all runs
+    actual macro will look similar to this:
+    {% macro generate_schema_name(custom_schema_name, node) -%}
+
+        {%- set dev_suffix = env_var('DEV_SCHEMA_SUFFIX', '') -%}
+
+        {%- if target.schema.startswith("wizard") -%}
+            {# temp: until we use new API connection and generic GH runners #}
+            {{ 'test_schema' }}
+        {%- elif target.name == 'prod' -%}
+            {# prod environment, writes to target schema #}
+            {{ target.schema }}
+        {%- elif target.name != 'prod' and dev_suffix != '' -%}
+            {# dev environments, writes to target schema with dev suffix #}
+            {{ target.schema }}__tmp_{{ dev_suffix | trim }}
+        {%- else -%}
+            {{ target.schema }}__tmp_
+        {%- endif -%}
+
+    {%- endmacro %}
+#}
+
 {% macro generate_schema_name(custom_schema_name, node) -%}
-
-    {%- set dev_suffix = env_var('DEV_SCHEMA_SUFFIX', '') -%}
-
-    {%- if target.schema.startswith("wizard") -%}
-        {# temp: until we use new API connection and generic GH runners #}
-        {{ 'test_schema' }}
-    {%- elif target.name == 'prod' -%}
-        {# prod environment, writes to target schema #}
-        {{ target.schema }}
-    {%- elif target.name != 'prod' and dev_suffix != '' -%}
-        {# dev environments, writes to target schema with dev suffix #}
-        {{ target.schema }}__tmp_{{ dev_suffix | trim }}
-    {%- else -%}
-        {{ target.schema }}__tmp_
-    {%- endif -%}
-
+    {{ 'test_schema' }}
 {%- endmacro %}
