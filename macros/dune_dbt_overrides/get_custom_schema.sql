fe72d6dc0b784236db003e012ad3ddd342417cb8
jeff-dude <jeff@dune.xyz>
2025-10-06 18:35:33 -0400

    remove from code block

--- macros/dune_dbt_overrides/get_custom_schema.sql
+++ macros/dune_dbt_overrides/get_custom_schema.sql
@@ -6,25 +6,6 @@
 
         
     TEMP: force `test_schema` for all runs
-    actual macro will look similar to this:
-    {% macro generate_schema_name(custom_schema_name, node) -%}
-
-        {%- set dev_suffix = env_var('DEV_SCHEMA_SUFFIX', '') -%}
-
-        {%- if target.schema.startswith("wizard") -%}
-            {# temp: until we use new API connection and generic GH runners #}
-            {{ 'test_schema' }}
-        {%- elif target.name == 'prod' -%}
-            {# prod environment, writes to target schema #}
-            {{ target.schema }}
-        {%- elif target.name != 'prod' and dev_suffix != '' -%}
-            {# dev environments, writes to target schema with dev suffix #}
-            {{ target.schema }}__tmp_{{ dev_suffix | trim }}
-        {%- else -%}
-            {{ target.schema }}__tmp_
-        {%- endif -%}
-
-    {%- endmacro %}
 #}
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
